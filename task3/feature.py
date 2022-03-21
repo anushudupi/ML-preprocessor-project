273f7af406a35a8d19e493b65d74725d680817d2
anushudupi <85610551+anushudupi@users.noreply.github.com>
2022-03-21 14:32:51 +0530

    Update feature.py

--- task3/feature.py
+++ task3/feature.py
@@ -3,13 +3,18 @@ import pandas as pd
 from PyInquirer import prompt as piq
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import PowerTransformer
+from sklearn.preprocessing import RobustScaler
+from sklearn.preprocessing import MaxAbsScaler
+from sklearn.preprocessing import QuantileTransformer
 
 questions = [
     {
         'type': 'list',
         'name': 'user_option',
         'message': 'choose type of feature scaling - ',
-        'choices': ["Normalisation", "Standardisation"]
+        'choices': ["Normalisation/MinMaxScaling", "Standardisation/z-score", "PowerTransformer", "RobustScaler",
+                    "MaxAbsScaler","QuantileTransformer"]
     }
 ]
 
@@ -22,16 +27,35 @@ def hello(f):
     answers = piq(questions)
     try:
         df = pd.read_csv(f)
-        for a in df[1:1]:
+        for a in df.columns:
             if df[a].dtypes == 'object':
                 df.drop([a], axis=1, inplace=True)
         df.dropna(inplace=True)
-        if answers.get("user_option") == "Normalisation":
-            norm = MinMaxScaler().fit(df)
-            print(norm.transform(df))
-        elif answers.get("user_option") == "Standardisation":
-            stds = StandardScaler().fit(df)
-            print(stds.transform(df))
+        if answers.get("user_option") == "Normalisation/MinMaxScaling":
+            scaler = MinMaxScaler().fit(df)
+            dfx = pd.DataFrame(scaler.transform(df), columns=df.columns)
+            print(dfx)
+        elif answers.get("user_option") == "Standardisation/z-score":
+            scaler = StandardScaler().fit(df)
+            dfx = pd.DataFrame(scaler.transform(df), columns=df.columns)
+            print(dfx)
+        elif answers.get("user_option") == "PowerTransformer":
+            scaler = PowerTransformer(method='yeo-johnson')
+            dfx = pd.DataFrame(scaler.fit_transform(df), columns=df.columns)
+            print(dfx)
+        elif answers.get("user_option") == "RobustScaler":
+            scaler = RobustScaler()
+            dfx = pd.DataFrame(scaler.fit_transform(df), columns=df.columns)
+            print(dfx)
+        elif answers.get("user_option") == "MaxAbsScaler":
+            scaler = MaxAbsScaler()
+            dfx = pd.DataFrame(scaler.fit_transform(df), columns=df.columns)
+            print(dfx)
+        elif answers.get("user_option") == "QuantileTransformer":
+            scaler = QuantileTransformer()
+            dfx = pd.DataFrame(scaler.fit_transform(df), columns=df.columns)
+
+            print(dfx)
 
     except:
         print("csv file not found")
